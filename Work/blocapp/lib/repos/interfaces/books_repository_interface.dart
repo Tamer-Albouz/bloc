880dd8ef584d17bf970514b36e71524056a0c3a9
Tamer Albouz <tameralbouz9@gmail.com>
2023-06-06 16:13:32 +0300

    ini

--- Work/blocapp/lib/repos/interfaces/books_repository_interface.dart
+++
@@ -1,8 +0,0 @@
-import 'package:blocapp/models/book_model.dart';
-
-abstract class BooksRepositoryInterface {
-  Future<List<BookModel>?> getBooks();
-  Future<void> patchBook(String id, body);
-  Future<String?> deleteBook(String id);
-  Future<void> postBook(body);
-}
